6fd71e771c78d1966ed62acaa96d58e1d513cf82
Aman <aman.swar001@gmail.com>
2025-01-15 18:54:06 +0530

    added DRijepa architecture experimental

--- data_pipeline/data_set.py
+++ data_pipeline/data_set.py
@@ -6,16 +6,18 @@ from torch.utils.data import Dataset , DataLoader
 from data_load import EyepacsGradingDataset , AptosGradingDataset , IdridGradingDataset , DdrGradingDataset
 import random
 from PIL import Image
+from typing import Tuple , List
+
 
 class UnitedTrainingDataset(Dataset):
 
-    def __init__(self , transformation=None , *args):
+    def __init__(self , *args , transformation=None):
         self.args = args
         self.image_path = []
         self.labels = []
         self.transformation = transformation
 
-        #concatinating all datasets
+        #appending all datasets
         for arg in args:
             img_path , label = self.__getdata(arg)
             self.image_path.extend(img_path)
@@ -24,35 +26,39 @@ class UnitedTrainingDataset(Dataset):
         #logic for shuffling
         img_path_label_pair = list(zip(self.image_path , self.labels))
         random.shuffle(img_path_label_pair)
-        self.image_path , self.labels = zip(*img_path_label_pair)
+        self.image_path , self.labels = map(list , zip(*img_path_label_pair))
 
-    def __getdata(self , dataset_name: str) -> list:
+    def __getdata(self , dataset_name: str) -> Tuple[List[str] , List[int]]:
 
-        if dataset_name == "eyepacs":
+        if dataset_name not in ["eyepacs" , "aptos" ,"ddr" , "idrid"]:
+            raise ValueError(f"Unknown dataset {dataset_name}")
+        
+
+        elif dataset_name.lower() == "eyepacs":
             eyepacs = EyepacsGradingDataset()
             eyepacs_train_img , eyepacs_train_labels = eyepacs.get_train_set()
             
             return eyepacs_train_img , eyepacs_train_labels
 
-        elif dataset_name == "aptos":
+        elif dataset_name.lower() == "aptos":
             aptos = AptosGradingDataset()
             aptos_train_img , aptos_train_labels = aptos.get_train_set()
             
             return aptos_train_img , aptos_train_labels
            
 
-        elif dataset_name == "ddr":
+        elif dataset_name.lower() == "ddr":
             ddr = DdrGradingDataset()
             ddr_train_img , ddr_train_labels = ddr.get_train_set()
             
             return ddr_train_img , ddr_train_labels
         
-        elif dataset_name == "idrid":
+        elif dataset_name.lower() == "idrid":
             idrid = IdridGradingDataset()
             idrid_train_img , idrid_train_labels = idrid.get_train_set()
             return idrid_train_img , idrid_train_labels
 
-    def get_paths(self) -> list:
+    def get_paths(self) -> Tuple[List[str] , List[int]]:
         """
         To get combined path of all images of given dataset and corresponding labels
         """
@@ -63,16 +69,19 @@ class UnitedTrainingDataset(Dataset):
         return len(self.image_path)
     
     def __getitem__(self, index):
-
+        
         img_path = self.image_path[index]
         label = self.labels[index]
 
-        img = Image.open(img_path)
+        try:
+            img = Image.open(img_path)
 
-        if self.transformation is not None:
-            img = self.transformation(img)
+            if self.transformation is not None:
+                img = self.transformation(img)
 
-        return img , label
+            return img , label
+        except Exception as e:
+            raise RuntimeError(f"error loading img {img_path}")
 
 
 class UnitedValidationDataset(Dataset):
---
+++ model/DRijepa.py
@@ -0,0 +1,241 @@
+import torch
+import torch.nn as nn
+from einops import rearrange
+import torch.nn.functional as F
+
+class Patchify(nn.Module):
+    """
+    convert images into patches , with hierachial project to support large img size
+    """
+
+    def __init__(self , img_size = 2048 , patch_size =32, in_chan=3 , embed_dim = 1024):
+        super().__init__()
+
+        self.img_size = img_size
+        self.path_size = patch_size
+        self.n_patches = (img_size // patch_size) ** 2
+
+        self.hierarch_proj = nn.Sequential(
+
+            #
+            nn.Conv2d(in_chan , embed_dim // 4 , kernel_size=7 , stride=2 , padding=3),
+            nn.LayerNorm([embed_dim // 4 , img_size // 2 , img_size //2]),
+            nn.GELU(),
+            nn.Conv2d(embed_dim // 4 , embed_dim // 2 , kernel_size=3 , stride=2 , padding=1),
+            nn.LayerNorm([embed_dim // 2 , img_size // 4 , img_size //4]),
+            nn.GELU(),
+            nn.Conv2d(embed_dim //2 , embed_dim , kernel_size=patch_size // 4 , stride=patch_size //4)
+
+            
+        )
+
+    def forward(self , x):
+        x = self.hierarch_proj(x) # -> batch channel height width
+        x = rearrange(x , 'b c h w -> b (h w) c')
+
+
+class TransformerEncoder(nn.Module):
+
+    def __init__(self , dim , depth , heads , mlp_dim , dropout=0.1):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        self.block = nn.ModuleList([
+            nn.LayerNorm(dim),
+            nn.MultiheadAttention(dim , heads , dropout=dropout),
+            nn.LayerNorm(dim),
+            nn.Sequential(
+                nn.Linear(dim , mlp_dim),
+                nn.GELU(),
+                nn.Dropout(dropout),
+                nn.Linear(mlp_dim , dim),
+                nn.Dropout(dropout)
+            )
+        ])
+        for _ in range(depth):
+            self.layers.append(self.block)
+
+    def forward(self,  x):
+
+        for norm1 , attn, norm2 , mlp in self.layers:
+            x_norm = norm1(x)
+            x_attn = attn(x_norm , x_norm ,x_norm)
+            x = x + x_attn
+            x_norm_2 = norm2(x)
+            x_mlp = mlp(x_norm_2)
+            x = x + x_mlp
+
+        return x
+    
+class DRIjepa(nn.Module):
+    def __init__(
+            self,
+            img_size = 2048, 
+            patch_size = 32 ,
+            in_chan = 3 , 
+            embed_dim = 1024 , 
+            encoder_depth = 12 , 
+            pred_depth = 4 ,
+            n_heads = 16 , 
+            mlp_ratio =4, 
+            drop = 0.1
+            ):
+        
+        super().__init__()
+
+        self.patch_embed = Patchify(img_size=img_size , patch_size=patch_size , in_chan=in_chan , embed_dim=embed_dim)
+
+        self.target_encoder = TransformerEncoder(
+            dim=embed_dim , 
+            depth=encoder_depth,
+            heads=n_heads,
+            mlp_dim= embed_dim * mlp_ratio,
+            dropout=drop
+        )
+
+
+        self.context_encoder = TransformerEncoder(
+            dim=embed_dim , 
+            depth=encoder_depth,
+            heads=n_heads,
+            mlp_dim= embed_dim * mlp_ratio,
+            dropout=drop
+        )
+
+
+        self.predictor = nn.Sequential(
+            TransformerEncoder(
+                dim=embed_dim , 
+                depth=encoder_depth,
+                heads=n_heads,
+                mlp_dim= embed_dim * mlp_ratio,
+                dropout=drop
+            ),
+            nn.LayerNorm(embed_dim),
+            nn.Linear(embed_dim , embed_dim)
+        )
+
+
+        self.apply(self._init_weights)
+
+        self.grid_size = img_size // patch_size
+
+    def _init_weights(self , m):
+
+        if isinstance(m , nn.Linear):
+            torch.nn.init.xavier_uniform_(m.weight)
+
+            if m.bias is not None:
+                nn.init.constant_(m.bias , 0)
+
+        elif isinstance(m , nn.LayerNorm):
+            nn.init.constant_(m.bias , 0)
+
+            nn.init.constant_(m.weight , 1)
+
+    
+    def get_random_boxes(self , batch_size , n_box=6):
+
+        boxes = []
+
+        for _ in range(n_box):
+            batch_wise_box = []
+            for _ in range(batch_size):
+                # to generate images more inclined to center
+                center_bias = torch.randn(2) * 0.5
+
+                x_center = self.grid_size // 2 + int(center_bias[0] * self.grid_size // 4)
+                y_center = self.grid_size // 2 + int(center_bias[1] * self.grid_size // 4)
+
+                w = torch.randint(4 , 8 , (1,)).item()
+                h = torch.randint(4 , 8 , (1,)).item()
+
+
+                x1 = max(0 , x_center - w // 2)
+                y1 = max(0 , y_center - h // 2)
+                x2 = min(self.grid_size , x1 + w)
+                y2 = min(self.grid_size , y1 + h)
+
+                batch_wise_box.append([x1 , y1 , x2 , y2])
+
+            boxes.append(batch_wise_box)
+
+
+        return torch.tensor(boxes)
+    
+    def extract_target(self , feature , boxes):
+        b ,c ,d = feature.shape
+        h = w = int(c ** 0.5)
+        feature = rearrange(feature , 'b (h w) d -> b d h w' , h=h)
+
+        target_feature = []
+        for _b in range(b):
+            batch_target = []
+
+            for box in boxes[b]:
+
+                x1 , y1 , x2 , y2 = box
+                target = feature[b:b+1 , : , y1:y2 , x1:x2]
+
+                target = F.adaptive_avg_pool2d(target , (2,2))
+                target = rearrange(target ,'b c h w -> b (h w) c')
+
+                batch_target.append(target)
+
+            target_feature.append(torch.cat(batch_target , dim=1))
+
+        return torch.stack(target_feature)
+    
+
+    def forward(self , images , boxes=None):
+        B = images.shape[0]
+
+        if boxes is None:
+            boxes = self.get_random_boxes(B)
+
+        x = self.patch_embed(images)
+
+
+        context_features = self.context_encoder(x)
+
+        with torch.no_grad():
+            target_feat = self.target_encoder(x)
+            target_feat = self.extract_target(target_feat , boxes)
+
+        predicted_feature = self.predictor(context_features)
+        predicted_feature = self.extract_target(predicted_feature , boxes)
+
+        return predicted_feature , target_feat
+
+         
+    
+def create_ijepa(
+        img_size = 2048,
+        patch_size = 32 , 
+        in_chan = 3 , 
+        embed_dim = 1024 ,
+        encoder_depth = 12,
+        pred_depth = 4,
+        n_heads = 16,
+        mlp_ratio = 4,
+        dropout = 0.1
+):
+    model = DRIjepa(
+        img_size=img_size,
+        patch_size=patch_size,
+        in_chan=in_chan,
+        embed_dim=embed_dim,
+        encoder_depth=encoder_depth,
+        pred_depth=pred_depth,
+        n_heads=n_heads,
+        mlp_ratio=mlp_ratio,
+        drop=dropout
+    )
+
+    loss = nn.MSELoss()
+    return model , loss
+
+
+
+
+        
+
