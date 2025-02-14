f01fafea9e6e32624e0d75d70be7445898d82157
Aman <aman.swar001@gmail.com>
2025-02-14 15:42:48 +0530

    swin trans config add

--- model/utils.py
+++ model/utils.py
@@ -29,6 +29,46 @@ vit_config = {
 
 }
 
+vit_test_config = {
+    "img_size" : 224,
+    "patch_size" : 16,
+    "in_chans" : 3,
+    "embed_dim" : 512,
+    "depth" : 3,
+    "num_heads" : 4,
+    "mlp_ratio" : 4,
+    "mask_ratio" : 0.4, #only for ibot
+    "num_regs" : 4, # only for dino with regs
+    "pred_depth" : 2
+
+}
+
+swin_test_config = {
+    "img_size": 256,
+    "patch_size": 4,
+    "embed_dim": 128,
+    "depths": [2, 2, 18, 2],
+    "num_heads": [4, 8, 16, 32],
+    "window_size": 7,
+    "num_regs": 4,
+    "transformer_embed_dim": 256,
+    "transformer_heads": 8,
+    "num_transformer_layers": 8
+}
+
+swin_config = {
+    "img_size": 512,
+    "patch_size": 4,
+    "embed_dim": 128,
+    "depths": [2, 2, 18, 2],
+    "num_heads": [4, 8, 16, 32],
+    "window_size": 7,
+    "num_regs": 8,
+    "transformer_embed_dim": 1024,
+    "transformer_heads": 16,
+    "num_transformer_layers": 4
+}
+
 """
 vit_config["img_size"]
 vit_config["patch_size"]
