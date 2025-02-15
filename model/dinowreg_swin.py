3ad464e63ae47a78a2d18249ef0943a0ed505716
aman <as2275@gmail.com>
2025-02-15 12:42:05 +0530

    minor fixes

--- model/dinowreg_swin.py
+++ model/dinowreg_swin.py
@@ -586,7 +586,7 @@ if __name__ == "__main__":
     train_loader = SSLTrainLoader(
         dataset_names=dataset_names,
         transformation=augmentor,
-        batch_size=32,
+        batch_size=48,
         num_work=4,
     ).get_loader()
 
@@ -599,5 +599,5 @@ if __name__ == "__main__":
 
     get_mem_info()
     max_epoch = 300
-    train_single_gpu(train_dl=train_loader, valid_dl=valid_loader, b_size=32, max_epoch=max_epoch)
+    train_single_gpu(train_dl=train_loader, valid_dl=valid_loader, b_size=48, max_epoch=max_epoch)
     # To run DDP training instead, call train_ddp(args)
