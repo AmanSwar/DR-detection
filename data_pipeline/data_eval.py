48dfd07f4676f7da2d1df885453b2d2ec1a3472e
Aman <aman.swar001@gmail.com>
2025-04-11 21:18:52 +0530

    Uniform Test set

--- data_pipeline/data_eval.py
+++ data_pipeline/data_eval.py
@@ -6,7 +6,8 @@ from data_pipeline.data_load import (
     AptosGradingDataset , 
     IdridGradingDataset,
     MessdrGradingDataset,
-    SustechDataset
+    SustechDataset,
+    DeepDrDataset
 )
 
 from PIL import Image
@@ -23,12 +24,12 @@ deepdr
 
 
 
-aptos_test_set = AptosGradingDataset()
-idirid_test_set = IdridGradingDataset()
-ddr_test_set = DdrGradingDataset(root_dir='data/ddr')
-messdr_test_set = MessdrGradingDataset()
-sustech_test_set = SustechDataset()
-
+# aptos_test_set = AptosGradingDataset()
+# idirid_test_set = IdridGradingDataset()
+# ddr_test_set = DdrGradingDataset(root_dir='data/ddr')
+# messdr_test_set = MessdrGradingDataset()
+# sustech_test_set = SustechDataset()
+# deepdr_ds = DeepDrDataset()
 
 class UniTestDataset(Dataset):
 
@@ -43,21 +44,21 @@ class UniTestDataset(Dataset):
 
     def _get_data(self):
         ds = self.dataset_name
+        # img = None
+        # label = None
+        # if ds == "aptos":
+        #     img, label = AptosGradingDataset().get_test_set()
 
-        if ds == "aptos":
-            img, label = AptosGradingDataset().get_test_set()
+        # elif ds == "idirid":
+        #     img, label = IdridGradingDataset().get_test_set()
 
-        elif ds == "idirid":
-            img, label = IdridGradingDataset().get_test_set()
-
-        elif ds == "ddr":
-            img, label = DdrGradingDataset(root_dir='data/ddr').get_test_set()
-        elif ds == "messdr":
-            img, label = MessdrGradingDataset().get_test_set()
-        elif ds == "sustech":
-            img, label = SustechDataset().get_test_set()
-
-
+        # elif ds == "ddr":
+        #     img, label = DdrGradingDataset(root_dir='data/ddr').get_test_set()
+        # elif ds == "messdr":
+        #     img, label = MessdrGradingDataset().get_test_set()
+        # elif ds == "sustech":
+        img, label = DeepDrDataset().get_test_set()
+        print(img[:5])
         self.images = img
         self.labels = label
 
@@ -65,17 +66,20 @@ class UniTestDataset(Dataset):
         return len(self.images)
 
     def __getitem__(self, index):
-        
         img_path = self.images[index]
         label = self.labels[index]
         img = Image.open(img_path)
-        trans_img = self.transform(img)
+        if self.transform:
+            trans_img = self.transform(img)
+            return trans_img , label
 
-        return trans_img , label
-    
+        else:
+            return img , label
+            
 
 
-class UniTestLoader(DataLoader):
+
+class UniTestLoader:
 
     def __init__(self , dataset_name , transforms , batch_size , num_worker):
 
@@ -96,6 +100,11 @@ class UniTestLoader(DataLoader):
     def get_loader(self):
         return self.dl
     
+if __name__ == "__main__":
+    ds_name = ["sustech"]
+    ds = UniTestDataset(ds_name , transform=None)
+
+    print(len(ds))
 
 
 
@@ -104,7 +113,6 @@ class UniTestLoader(DataLoader):
 
 
 
-
-
+print(len(UniTestDataset("asd" , None)))
 
 
